b14f90a5cabd29b847d27d1772e79c65489f702b
GeanCarlos <cgean3385@alu.ufc.br>
2025-07-23 16:49:55 -0300

    teste github

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -7,8 +7,6 @@ export default function HomePage() {
       <h1>Bem-vindo à tela inicial!</h1>
       {/* Adicione o conteúdo da sua tela inicial aqui */}
       <p>Você está logado.</p>
-      <p>Testes para github</p>
-      <p>MAIS UM </p>
     </div>
   );
 }
